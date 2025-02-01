77514812dc9dd2820ce64c8f795722eb8da813ff
Linus Norton <linusnorton@gmail.com>
2025-02-01 07:35:56 +0000

    more camera options

--- src/main.js
+++ src/main.js
@@ -6,8 +6,13 @@ var video = document.getElementById('video');
 
 // Get access to the camera!
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Not adding `{ audio: true }` since we only want video now
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+    const options = {
+        video: {
+            facingMode: 'environment'
+        },
+        audio: false
+    };
+    navigator.mediaDevices.getUserMedia(options).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
         video.srcObject = stream;
         video.play();
